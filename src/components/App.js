fddd9012724ce272070b0e02a602060dc4721588
Freewhisper77 <emmasddx@foxmail.com>
2022-11-18 15:51:41 +0800

    feat: [T-1] Use props to replace hardcode data in Note

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      <Note title={notes[0].title} content={notes[0].content} />
+      <Note title={notes[1].title} content={notes[1].content} />
       <Footer />
     </div>
   );
